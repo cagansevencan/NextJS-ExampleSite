5ab5e5519a4b2c3c888773adccbc89f04ebc0d24
Cagan Sevencan <cagansvncn@gmail.com>
2020-09-26 19:17:40 +0300

    Spotify Playlist Integration

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Layout from '../components/layout'
 import Head from "next/head";
 import unfetch from "isomorphic-unfetch";
+import { Buffer } from "buffer";
+import APIController from '../server/spotify-server'
 
-function HomePage() {
+function HomePage({playlists}) {
   return (
     <Layout>
         <Head>
@@ -10,88 +12,33 @@ function HomePage() {
         </Head>
       <h1>Welcome to my first website!</h1>
 
+      <h2>Here are my Spotify playlists</h2>
 
+      <ul>
+      {playlists.items.map(playlist =>
+        (
+          <li key={playlist.id}>{playlist.name}</li>
 
-
-        <style jsx>{`
-      `}
-        </style>
-
-
+        ))}
+      </ul>
     </Layout>
   )
 }
 
-const APIController = (function()  {
-  const clientId = ' ';
-  const clientSecret = ' ';
-
-  //private methods
-  const _getToken = async () => {
-    const result = await fetch(' https://accounts.spotify.com/api/token',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type' : 'application/x-www-form-urlencoded',
-          'Authorization' : 'Basic ' +btoa(clientId + ':' + clientSecret)  //base64 encoded representation
-        },
-        body: 'grant_type=client_credentials'
-      });
-    const data =  await result.json();
-    return data.access_token;  //Bearer token returned
-  }
-
-const _getPlaylistsByUser = async (token) => {
-  const result = await fetch('https://api.spotify.com/v1/users/1299726247/playlists?offset=0&limit=20' , {
-    method: 'GET',
-    headers: { 'Authorization' : 'Bearer ' + token}
-  });
-  const data = await result.json();
-  return data.playlists.items;
-}
-
-const _getTracks = async (token, tracksEndPoint) => {
-  const limit = 20;
-  const result = await fetch(`${tracksEndPoint}?limit=${limit}`, {
-    method: 'GET',
-    headers: { 'Authorization' : 'Bearer ' + token}
-  });
-  const data = await result.json();
-  return data.items;
-}
-
-const _getTrack = async (token, trackEndPoint) => {
-  const result = await fetch(`${trackEndPoint}`, {
-    method: 'GET',
-    headers: { 'Authorization' : 'Bearer ' + token}
-  });
-  const data = await result.json();
-  return data;
-}
-
-  return {
-    getToken() {
-      return _getToken();
-    },
-    getPlaylistByUser(token) {
-      return _getPlaylistsByUser(token);
-    },
-    getTracks(token, tracksEndPoint) {
-      return _getTracks(token, tracksEndPoint);
-    },
-    getTrack(token, trackEndPoint) {
-      return _getTrack(token, trackEndPoint);
-    }
-  }
-}) ();  //Fire the function immediately
 
 
 
 export async function getStaticProps() {
+
+    const token = await APIController.getToken();
+  console.log("HERE");
+    console.log(token)
+    const playlists = await APIController.getPlaylistByUser(token);
+  console.log(playlists);
   // data fetch
   return {
     props: {
-
+      playlists
     },
   }
 }
---
+++ server/spotify-server.js
@@ -0,0 +1,65 @@
+import { Buffer } from "buffer";
+const APIController = (function()  {
+
+
+  //private methods
+  const _getToken = async () => {
+    const result = await fetch(' https://accounts.spotify.com/api/token',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type' : 'application/x-www-form-urlencoded',
+          'Authorization' : 'Basic ' +( new Buffer(process.env.clientId + ':' + process.env.clientSecret).toString('base64')) //base64 encoded representation
+        },
+        body: 'grant_type=client_credentials'
+      });
+    const data =  await result.json();
+    return data.access_token; //Bearer token returned
+  }
+
+  const _getPlaylistsByUser = async (token) => {
+    const result = await fetch('https://api.spotify.com/v1/users/1299726247/playlists?offset=0&limit=20' , {
+      method: 'GET',
+      headers: { 'Authorization' : 'Bearer ' + token}
+    });
+    const data = await result.json();
+    return data;
+  }
+
+  const _getTracks = async (token, tracksEndPoint) => {
+    const limit = 20;
+    const result = await fetch(`${tracksEndPoint}?limit=${limit}`, {
+      method: 'GET',
+      headers: { 'Authorization' : 'Bearer ' + token}
+    });
+    const data = await result.json();
+    return data.items;
+  }
+
+  const _getTrack = async (token, trackEndPoint) => {
+    const result = await fetch(`${trackEndPoint}`, {
+      method: 'GET',
+      headers: { 'Authorization' : 'Bearer ' + token}
+    });
+    const data = await result.json();
+    return data;
+  }
+
+//Closures
+  return {
+    getToken() {
+      return _getToken();
+    },
+    getPlaylistByUser(token) {
+      return _getPlaylistsByUser(token);
+    },
+    getTracks(token, tracksEndPoint) {
+      return _getTracks(token, tracksEndPoint);
+    },
+    getTrack(token, trackEndPoint) {
+      return _getTrack(token, trackEndPoint);
+    }
+  }
+}) ();  //Fire the function immediately
+
+export default APIController
