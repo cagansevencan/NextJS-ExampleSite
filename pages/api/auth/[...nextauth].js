8ac5ba4a73df4ac263f3397c4a687c1eee4784b3
Cagan Sevencan <cagansvncn@gmail.com>
2020-10-04 14:04:00 +0300

    Deploy 5

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,7 +2,7 @@ import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
 const options = {
-  site: 'https://next-js-example-site.vercel.app/',
+  site: process.env.SITE ,
 
   // Configure one or more authentication providers
   providers: [
